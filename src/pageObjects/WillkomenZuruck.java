4230fa491cba94fa6a1cb28a6bf9f62d8a43aef4
tomicradmila <rtomic@itekako.com>
2019-03-15 15:58:03 +0100

    work on willkomen zuruck

--- src/pageObjects/WillkomenZuruck.java
+++ src/pageObjects/WillkomenZuruck.java
@@ -1,7 +1,6 @@
 package pageObjects;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,16 +12,53 @@ public class WillkomenZuruck {
 	public static WebElement dismissButton (WebDriver driver) {
 		
 		WebDriverWait wait = new WebDriverWait(driver, 5);
-		
+		WebElement dismissButton;
 		try {
-			WebElement dismissButton=wait.until(ExpectedConditions.elementToBeClickable(By.id("drupal-modal")));
+			 dismissButton=wait.until(ExpectedConditions.elementToBeClickable(By.id("drupal-modal")));
 		}
 		
 		catch(Exception e) {
 			return null;
 		}
 				
-		return null;				
+		return dismissButton;				
 	}
+	
+public static WebElement username (WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		WebElement username;
+		try {
+			 username=wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
+		}
+		catch(Exception e) {
+			return null;
+		}
+		return username;				
+	}
+
+
+public static WebElement password (WebDriver driver) {
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	WebElement password;
+	try {
+		 password=wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));
+	}
+	catch(Exception e) {
+		return null;
+	}
+	return password;				
+}
+
+public static WebElement merkenCheckbox (WebDriver driver) {
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	WebElement merkenCheckbox;
+	try {
+		 merkenCheckbox=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[contains(@id, 'edit-remember-me')]")));
+	}
+	catch(Exception e) {
+		return null;
+	}
+	return merkenCheckbox;				
+}
 
 }

a94e2ad27f1362c8c1ffd5525099659ca55fd6aa
tomicradmila <rtomic@itekako.com>
2019-03-18 17:25:22 +0100

    found elements on willkomen zuruck

--- src/pageObjects/WillkomenZuruck.java
+++ src/pageObjects/WillkomenZuruck.java
@@ -9,6 +9,24 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WillkomenZuruck {
+	
+	//1
+	public static WebElement title (WebDriver driver) {
+		
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		WebElement title;
+		try {
+			 title=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), 'Willkommen')]")));
+		}
+		
+		catch(Exception e) {
+			return null;
+		}
+				
+		return title;				
+	}
+	
+	//2
 	public static WebElement dismissButton (WebDriver driver) {
 		
 		WebDriverWait wait = new WebDriverWait(driver, 5);
@@ -24,6 +42,7 @@ public class WillkomenZuruck {
 		return dismissButton;				
 	}
 	
+	//3
 public static WebElement username (WebDriver driver) {
 		WebDriverWait wait = new WebDriverWait(driver, 5);
 		WebElement username;
@@ -36,7 +55,7 @@ public static WebElement username (WebDriver driver) {
 		return username;				
 	}
 
-
+//4
 public static WebElement password (WebDriver driver) {
 	WebDriverWait wait = new WebDriverWait(driver, 5);
 	WebElement password;
@@ -49,6 +68,7 @@ public static WebElement password (WebDriver driver) {
 	return password;				
 }
 
+//5
 public static WebElement merkenCheckbox (WebDriver driver) {
 	WebDriverWait wait = new WebDriverWait(driver, 5);
 	WebElement merkenCheckbox;
@@ -59,6 +79,46 @@ public static WebElement merkenCheckbox (WebDriver driver) {
 		return null;
 	}
 	return merkenCheckbox;				
+}
+
+//6
+
+public static WebElement passwortVergessen (WebDriver driver) {
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	WebElement passwortVergessen;
+	try {
+		 passwortVergessen=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@id, 'edit-forget-password')]")));
+	}
+	catch(Exception e) {
+		return null;
+	}
+	return passwortVergessen;				
+}
+
+//7
+public static WebElement anmeldenButton (WebDriver driver) {
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	WebElement anmeldenButton;
+	try {
+		 anmeldenButton=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[contains(@id, 'edit-submit')]")));
+	}
+	catch(Exception e) {
+		return null;
+	}
+	return anmeldenButton;				
+}
+
+//8
+public static WebElement registrierenButton (WebDriver driver) {
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	WebElement registrierenButton;
+	try {
+		 registrierenButton=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@id, 'edit-register')]")));
+	}
+	catch(Exception e) {
+		return null;
+	}
+	return registrierenButton;				
 }
 
 }
